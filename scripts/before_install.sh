6261ec3c6d1cfcddaedc2168ba532f3c708db869
Aidan Militzer <115504926+avm-123@users.noreply.github.com>
2025-10-27 13:10:15 -0400

    Change app directory path for deployment

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,7 +2,7 @@
 # scripts/before_install.sh
 
 # Create app directory if it doesn't exist
-mkdir -p /home/ubuntu/app
+mkdir -p /home/ec2-user/app
 
 # Clean up old deployment files (optional)
 # rm -rf /home/ubuntu/app/*
